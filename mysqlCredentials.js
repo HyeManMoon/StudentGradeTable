b2d0b583ee793d371d4b94c91cbeb40b29fe295a
HyeManMoon <hyemanmoon@gmail.com>
2018-04-09 18:11:55 -0700

    committing master to new repo

---
+++ mysqlCredentials.js
@@ -0,0 +1,9 @@
+const credentials = {
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'sgt', //mysql access do tonight port: 8889,
+    port: 8889
+}
+
+module.exports = credentials;
